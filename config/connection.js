13e71f677b39e1bf9437e6b49af5667a5c5aadb4
b1acksmyth <smith.spence13@gmail.com>
2020-09-13 15:50:04 -0700

    made changes to dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql");
-require("dotenv").config();
+require('dotenv').config();
+//console.log(process.env);
 
-const connection = mysql.connection({
-  host: localhost,
+
+const connection = mysql.createConnection({
+  host: "localhost",
   port: 3306,
   user: "root",
-  password: process.env.password,
+  password: process.env.SQL_PWD,
   database: "employee_db"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected to Database at " + connection.threadId + "\n");
-  start();
 });
 
 module.exports = connection;
