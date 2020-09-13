8e7f0bb889d622600a25b8b4dfe20b797312b50c
b1acksmyth <smith.spence13@gmail.com>
2020-09-13 12:00:48 -0700

    update to no loger have express

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const express = require("express");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql");
 require("dotenv").config();
 
-const app = express();
 
